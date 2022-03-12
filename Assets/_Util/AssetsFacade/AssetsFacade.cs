30c9ce85ad1aae10bcfdf98ae5cb3f3eb85d08cb
ito-takao <takao.ito.k@gmail.com>
2022-03-12 12:17:47 +0900

    自作ライブラリを追加

---
+++ Assets/_Util/AssetsFacade/AssetsFacade.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace _Util
+{
+    public class AssetsFacade : MonoBehaviour
+    {
+        public static AssetsFacade Current { get; private set; }
+
+        [SerializeField]
+        private UnityEvent OnSetup = new UnityEvent();
+        [SerializeField]
+        private UnityEvent OnUpdate = new UnityEvent();
+        [SerializeField]
+        private UnityEvent OnFixedUpdate = new UnityEvent();
+        [SerializeField]
+        private UnityEvent OnLateUpdate = new UnityEvent();
+        [SerializeField]
+        private UnityEvent OnExit = new UnityEvent();
+
+        [System.Serializable]
+        private class EventClass
+        {
+            public string Name;
+            public UnityEvent Event;
+        }
+        [SerializeField]
+        private EventClass[] utilityEvents;
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            SetInspectorUI();
+        }
+
+        public void SetInspectorUI()
+        {
+            OnSetup = new UnityEvent();
+            foreach (IAssetsFacadeSetup i in FindObjectOfInterfaces<IAssetsFacadeSetup>())
+            {
+                UnityEditor.Events.UnityEventTools.AddPersistentListener(OnSetup, i.OnSetup);
+            }
+
+            OnUpdate = new UnityEvent();
+            foreach (IAssetsFacadeUpdate i in FindObjectOfInterfaces<IAssetsFacadeUpdate>())
+            {
+                UnityEditor.Events.UnityEventTools.AddPersistentListener(OnUpdate, i.OnUpdate);
+            }
+
+            OnFixedUpdate = new UnityEvent();
+            foreach (IAssetsFacadeFixedUpdate i in FindObjectOfInterfaces<IAssetsFacadeFixedUpdate>())
+            {
+                UnityEditor.Events.UnityEventTools.AddPersistentListener(OnFixedUpdate, i.OnFixedUpdate);
+            }
+
+            OnLateUpdate = new UnityEvent();
+            foreach (IAssetsFacadeLateUpdate i in FindObjectOfInterfaces<IAssetsFacadeLateUpdate>())
+            {
+                UnityEditor.Events.UnityEventTools.AddPersistentListener(OnLateUpdate, i.OnLateUpdate);
+            }
+
+            OnExit = new UnityEvent();
+            foreach (IAssetsFacadeExit i in FindObjectOfInterfaces<IAssetsFacadeExit>())
+            {
+                UnityEditor.Events.UnityEventTools.AddPersistentListener(OnExit, i.OnExit);
+            }
+        }
+#endif
+
+        private void Awake()
+        {
+            Current = this;
+        }
+
+        private void OnDestroy()
+        {
+            Current = (this);
+        }
+
+        public void InvokeUtilityEvnet(string eventName)
+        {
+            EventClass[] tmpArray = utilityEvents.Where(o => o.Name.IndexOf(eventName) >= 0).ToArray();
+
+            if (tmpArray.Length == 0)
+            {
+                throw new ArgumentException(string.Format("値が見つかりません　値[ \"{0}\"", eventName));
+            }
+
+            EventClass findEvent = tmpArray[0];
+            findEvent.Event.Invoke();
+        }
+
+        public void InvokeSetupAssets()
+        {
+            OnSetup.Invoke();
+        }
+
+        public void InvokeUpdateAssets()
+        {
+            OnUpdate.Invoke();
+        }
+
+        public void InvokeFixedUpdateAssets()
+        {
+            OnFixedUpdate.Invoke();
+        }
+
+        public void InvokeLateUpdateAssets()
+        {
+            OnLateUpdate.Invoke();
+        }
+
+        public void InvokExitAssets()
+        {
+            OnExit.Invoke();
+        }
+
+        private T[] FindObjectOfInterfaces<T>() where T : class
+        {
+            List<T> list = new List<T>();
+            foreach (var n in GameObject.FindObjectsOfType<Component>(true))
+            {
+                if (n is T component)
+                {
+                    list.Add(component);
+                }
+            }
+
+            T[] ret = new T[list.Count];
+            int count = 0;
+            foreach (T component in list)
+            {
+                ret[count] = component;
+                count++;
+            }
+            return ret;
+        }
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(AssetsFacade))]
+    public class AssetsFacadeEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("Set Componets"))
+            {
+                AssetsFacade t = target as AssetsFacade;
+                t.SetInspectorUI();
+            }
+        }
+    }
+#endif
+}
---
+++ Assets/_Util/GameState/Base/BaseState.cs
@@ -0,0 +1,92 @@
+﻿using Audio;
+using UnityEngine;
+using _Util;
+using _Util.GameState;
+using UnityEngine.SceneManagement;
+
+namespace _Application
+{
+    public abstract class BaseState : GameState<StateMachine>
+    {
+        protected AudioManager AudioManager => AudioManager.Current;
+        protected StageLoader StageLoader => StageLoader.Current;
+        protected AssetsFacade AssetsFacade => AssetsFacade.Current;
+
+        public static StageId LoadStageId { get; private set; } = StageId.None;
+        public static bool IsLoad => (LoadStageId != StageId.None);
+
+        public BaseState(StateMachine stateMachine) : base(stateMachine) { }
+
+        public override void OnStateEnter()
+        {
+            Debug.Log("LoadState : " + GetType().Name);
+
+            InitializeAllObjects();
+
+            AssetsFacade.InvokeSetupAssets();
+        }
+
+        protected void InitializeAllObjects()
+        {
+            AudioManager.Voice.Stop();
+            AudioManager.BGM.Stop();
+            AudioManager.Ambient.Stop();
+            AudioManager.SE.Stop();
+
+            LoadStageId = StageId.None;
+        }
+
+        public override void OnStateUpdate()
+        {
+            if (AssetsFacade.Current)
+            {
+                AssetsFacade.InvokeSetupAssets();
+            }
+        }
+
+        public override void OnStateFixedUpdate()
+        {
+            if (AssetsFacade.Current)
+            {
+                AssetsFacade.InvokeFixedUpdateAssets();
+            }
+        }
+
+        public override void OnStateLateUpdate()
+        {
+            base.OnStateLateUpdate();
+
+            if (AssetsFacade.Current)
+            {
+                AssetsFacade.InvokeLateUpdateAssets();
+            }
+        }
+
+        public override void OnStateExit()
+        {
+            if (AssetsFacade.Current)
+            {
+                AssetsFacade.InvokExitAssets();
+            }
+        }
+
+        protected void ReloadScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        public static void LoadStage(StageId stageId)
+        {
+            LoadStageId = stageId;
+        }
+
+        public static BaseState LoadState(StageId stageId, StateMachine stateMachine)
+        {
+            return stageId switch
+            {
+                StageId.None => new TemplateState(stateMachine),
+                _ => throw new System.ComponentModel.InvalidEnumArgumentException(),
+            };
+        }
+    }
+}
